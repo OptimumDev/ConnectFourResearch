1e67cb0132ace6e6aa52a6d8a74046099cac4d0e
Imessal <38953768+Imessal@users.noreply.github.com>
2020-05-24 19:54:16 +0500

    now considering opponent

--- ConnectFourResearch/Solvers/GreedySolver.cs
+++ ConnectFourResearch/Solvers/GreedySolver.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using ConnectFourResearch.Algorithms;
 using ConnectFourResearch.ConnectFour;
+using ConnectFourResearch.Extensions;
 
 namespace ConnectFourResearch.Solvers
 {
@@ -24,11 +25,14 @@ namespace ConnectFourResearch.Solvers
             foreach(var move in gameState.GetPossibleMoves())
             {
                 var newState = gameState.Move(move, player);
-                var score = GetEstimateScore(newState, me);
+                var score = GetEstimateScore(newState);
                 
                 yield return new Move(move, score);
             }
         }
+
+        private double GetEstimateScore(Board board) =>
+            GetEstimateScore(board, me) - GetEstimateScore(board, me.GetOpponent());
         
         private static double GetEstimateScore(Board board, Cell player) =>
             board.GetLinesCountOfLength(4, player) * 100000 +
